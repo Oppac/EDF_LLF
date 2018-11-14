a0525ad1f82ba7a451603cd11bc262acf5f3820d
Oppac <oppac26@gmail.com>
2018-11-14 14:27:46 +0100

    output & non recursive generator

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import time
 from numpy import lcm
 
 def get_data(input_file, offsets, wcets, periods):
@@ -39,19 +40,33 @@ def edf_utility(wcets, periods):
     return interval
 
 
-def draft_generator(tasks):
+def draft_generator(tasks, goal_utility, output):
+    offsets, wcets, periods, gen_utility = new_system(tasks)
+    while not (gen_utility > (goal_utility-0.01) and gen_utility < (goal_utility+0.01)):
+        offsets, wcets, periods, gen_utility = new_system(tasks)
+    print(offsets, wcets, periods)
+    output_system(offsets, wcets, periods, output)
+
+def output_system(offsets, wcets, periods, output):
+    try:
+        with open(output, 'w+') as file:
+            for line in range(len(offsets)):
+                file.write("{}; {}; {}\n".format(
+                offsets[line], wcets[line], periods[line]))
+    except OSError:
+        print('cannot open', output)
+
+
+def new_system(tasks):
     offsets = []
     wcets = []
     periods = []
     for i in range(tasks):
-        offsets.append(random.randint(0, 10))
-        wcets.append(random.randint(0, 50))
-        periods.append(random.randint(50, 100))
-        utility = edf_utility(wcets, periods)
-    if utility > 0.65 and utility < 0.75:
-        print(offsets, wcets, periods)
-    else:
-        draft_generator(tasks)
+        offsets.append(random.randint(0, 2))
+        wcets.append(random.randint(1, 3))
+        periods.append(random.randint(8, 20))
+        gen_utility = edf_utility(wcets, periods)
+    return offsets, wcets, periods, gen_utility
 
 def main():
     offsets = []
@@ -60,16 +75,22 @@ def main():
 
     if len(sys.argv) > 1:
         if (str(sys.argv[1]) == "edf_interval"):
-            get_data(sys.argv[2], offsets, wcets, periods)
-            priorities = get_priorities(periods[:])
-            interval = edf_feasibility_interval(offsets, periods)
+            if len(sys.argv) > 2:
+                get_data(sys.argv[2], offsets, wcets, periods)
+                priorities = get_priorities(periods[:])
+                interval = edf_feasibility_interval(offsets, periods)
+                print("{},{}".format(0, interval))
+            else:
+                print("Usage: python edf_inteval input_file")
+        elif (str(sys.argv[1]) == "gen"):
+            if len(sys.argv) > 4:
+                nb_tasks = int(sys.argv[2])
+                goal_utility = float(sys.argv[3])/100
+                output_file = sys.argv[4]
+                draft_generator(nb_tasks, goal_utility, output_file)
+            else:
+                print("Usage: python project.py gen nb_tasks utility output")
 
-            print(offsets, wcets, periods)
-            print(priorities)
-            print("{},{}".format(0, interval))
-    else:
-        draft_generator(6)
-
-
-
+start_time = time.time()
 main()
+print("--- %s seconds ---" %(time.time() - start_time))
